9974acf63fc1037c6aaa988e5cc264afb9d1a563
George Brighton <george@gebn.co.uk>
2017-04-30 00:41:39 +0100

    Create bare project

---
+++ README.rst
@@ -0,0 +1,24 @@
+nibble
+======
+
+.. image:: https://img.shields.io/pypi/status/nibble.svg
+   :target: https://pypi.python.org/pypi/nibble
+.. image:: https://img.shields.io/pypi/v/nibble.svg
+   :target: https://pypi.python.org/pypi/nibble
+.. image:: https://img.shields.io/pypi/pyversions/nibble.svg
+   :target: https://pypi.python.org/pypi/nibble
+.. image:: https://travis-ci.org/gebn/nibble.svg?branch=master
+   :target: https://travis-ci.org/gebn/nibble
+.. image:: https://coveralls.io/repos/github/gebn/nibble/badge.svg?branch=master
+   :target: https://coveralls.io/github/gebn/nibble?branch=master
+.. image:: https://landscape.io/github/gebn/nibble/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/gebn/nibble/master
+
+Speed, distance and time calculations around quantities of digital information. Coming soon.
+
+Installation
+------------
+
+::
+
+    $ pip install nibble
---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+import codecs
+
+import nibble
+
+
+def _read_file(name, encoding='utf-8'):
+    """
+    Read the contents of a file.
+
+    :param name: The name of the file in the current directory.
+    :param encoding: The encoding of the file; defaults to utf-8.
+    :return: The contents of the file.
+    """
+    with codecs.open(name, encoding=encoding) as f:
+        return f.read()
+
+
+setup(
+    name='nibble',
+    version=nibble.__version__,
+    description='Speed, distance and time calculations around quantities of digital information.',
+    long_description=_read_file('README.rst'),
+    license='MIT',
+    url='https://github.com/gebn/nibble',
+    author='George Brighton',
+    author_email='oss@gebn.co.uk',
+    packages=find_packages(),
+    install_requires=[
+        'six>=1.9.0'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose'
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points={
+        'console_scripts': [
+            'nibble = nibble.__main__:main_cli',
+        ]
+    }
+)
