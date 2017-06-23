a817391ab0b012ab76af553abcce23e63efa88d4
George Brighton <george@gebn.co.uk>
2017-06-23 19:28:25 +0100

    Resolve FIXME fixed in d3089e5, and another case requiring no modification

--- lexer.py
+++ lexer.py
@@ -23,15 +23,15 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # INPUT = '10Gb per second in TiB per hour'  # 'per' and '/' as PER terminals
 # INPUT = '10Gb/s for 11 minutes'  # information : speed FOR duration
 # INPUT = '11 minutes at 10Gb/s'  # information : duration AT speed
+# INPUT = '10Gb/s for 11 minutes at 5Gb/s'  # nesting
+# INPUT = '481MB at 15Mb/s in s'  # nesting
 
 # INPUT = '10Gb/s in GiB/s in KiB/s in MiB/s'  # FIXME - a conversion should produce a FormattedQuantity, which can be reformatted by subsequent conversions
 # TODO parser.parse(INPUT) should return a non-string that is converted into a string; this allows easier chaining internally, e.g. multiple conversions
 
-# INPUT = '10Gb/s for 11 minutes at 5Gb/s'  # FIXME (nesting)
 # INPUT = '10Gb/s in MiB/2m'  # FIXME (valued duration time for speed_unit)
 
 # INPUT = '481MB at 15Mb/s'  # TODO print durations in terms of years, months, weeks, days etc. by default "4 minutes 16.53 seconds"
-# INPUT = '481MB at 15Mb/s in s'  # TODO "256.5 s" (does this already work?)
 
 # INPUT = '10Gb/s in /w'  # PONDERING (grammar too strict - look back at valid `format_spec`s)
 # INPUT = '10Gb/s in MiB'  # PONDERING (same reason as above)
